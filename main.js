cffb07bbb41832fdbdaedcfa0d5d505da6586287
jpablopachar <jpablopachar1993@gmail.com>
2017-11-08 09:11:01 -0500

    Captura los elementos que se agregan por el usuario

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const electron = require('electron');
 const url = require('url');
 const path = require('path');
-const {app, BrowserWindow, Menu} = electron;
+const {app, BrowserWindow, Menu, ipcMain} = electron;
 
 let mainWindow;
 let addWindow;
@@ -47,6 +47,13 @@ function createAddWindow() {
         addWindow = null;
     });
 }
+
+// Captura los elemento que se agregan
+ipcMain.on('item:add', function(e, item) {
+    console.log(item);
+    mainWindow.webContents.send('item:add', item);
+    addWindow.close();
+})
 
 // Creación del menú principal
 const mainMenuTemplate = [
